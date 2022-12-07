3561769e2947cf5e32dd4a7a812d5ad6ba2f1165
andrew <AFerDEV17@gmail.com>
2022-12-06 20:23:14 -0700

    Learning Python

--- script.py
+++ script.py
@@ -1,2 +1,39 @@
-# Let's create a Colorado Avalanche game schedule and reminder program.
+# Learning Python
+# Make a script to automate display configuration settings on startup.
+AVS = "Colorado Avalanche \n    2022-2023\n"
+print(AVS)
 
+AVS, YSN, Y22, Y23 = "Colorado Avalanche", "2022-2023", "2022", "2023"
+print(AVS)
+print(YSN)
+print(Y22)
+print(Y23)
+
+years = ("2022-2023", "2022", "2023")
+YSN, Y22, Y23 = years
+print(YSN)
+print(Y22)
+print(Y23)
+
+x = "awesome"
+
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+def myfunc():
+    global x
+    x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
